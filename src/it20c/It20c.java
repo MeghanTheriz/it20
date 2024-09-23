5b0af946b45e0dc07d97e78a58dd8f048e687221
MeghanTheriz <twinklerautraut711@gmail.com>
2024-09-23 11:44:58 +0800

    Adding Comments To Code

--- src/it20c/It20c.java
+++ src/it20c/It20c.java
@@ -2,17 +2,20 @@
 package it20c;
 
 public class It20c {
-
-   
+  
     public static void main(String[] args) {
+        //This one is intended for row limit
        int row = 5;
+       //This For Loop is for the row
        for(int i = 0; i<row; i++){
+            //This For Loop is for the column
            for(int j = 0; j<row-i-1;j++){
                System.out.print(" ");
            }
            for(int j=0;j<i;j++) {
            System.out.print(" *");
            }
+            //This Println is to next line
            System.out.println(" ");
        }
        
